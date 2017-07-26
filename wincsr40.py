dc0aa1830e1ea6023ca6759b7a930fe86bc69c15
sunzanfeng <sun.zanfeng@163.com>
2017-07-26 15:37:20 +0800

    Update wincsr40.py

--- wincsr40.py
+++ wincsr40.py
@@ -1,8 +1,17 @@
-"""Test2: PL2303 serial port user-space driver using WINUSB high level api"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("./WinUsbPy")
+sys.path.append("./WinUsbPy/winusbpy")
+import logging
+import argparse
 import time
-from winusbpy import *
-pl2303_vid = "067b"
-pl2303_pid = "2303"
+from WinUsbPy.winusbpy.winusbpy import *
+from WinUsbPy.winusbpy.winusb import *
+from WinUsbPy.winusbpy.winusbclasses import *
+pl2303_vid = "0a12"
+pl2303_pid = "0001"
 
 """ USB Setup Packets """
 pkt1 = UsbSetupPacket(0xc0, 0x01, 0x8484, 0x00, 0x01)
@@ -28,57 +37,57 @@ pkt19 = UsbSetupPacket(0xc0, 0x01, 0x0081, 0x00, 0x02)
 pkt20 = UsbSetupPacket(0x40, 0x01, 0x0000, 0x01, 0x00)
 
 """ USB Data """
-hello = "Hello"
-header = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00\x00\x01\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00"
-tx1 = "\x18"
-tx2 = "\x08"
-tx3 = "\x08"
-tx4 = "\x14"
-tx5 = "\x14"
-tx6 = "\x22"
-tx7 = "\x3e"
-tx8 = "\x22"
-tx9 = "\x77"
-tx10 = "\x00"
-tx11 = "\x00"
-tx12 = "\x00"
+hello = b"Hello"
+header = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00\x00\x01\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00\x08\x01\x00\x00"
+tx1 = b"\x18"
+tx2 = b"\x08"
+tx3 = b"\x08"
+tx4 = b"\x14"
+tx5 = b"\x14"
+tx6 = b"\x22"
+tx7 = b"\x3e"
+tx8 = b"\x22"
+tx9 = b"\x77"
+tx10 = b"\x00"
+tx11 = b"\x00"
+tx12 = b"\x00"
 
 """ WinUsbPy  """
 
 api = WinUsbPy()
 result = api.list_usb_devices(deviceinterface=True, present=True)
 if result > 0:
-	if api.init_winusb_device(pl2303_vid, pl2303_pid):
-		speed = api.query_device_info(query=1)
+        if api.init_winusb_device(pl2303_vid, pl2303_pid):
+                speed = api.query_device_info(query=1)
 		if speed != -1:
-			print "Device Speed: " + str(speed)
-		else:
-			print "Device speed could not be obtained"
+                        print(("Device Speed: " + str(speed)))
+                else:
+                        print("Device speed could not be obtained")
 
 		interface_descriptor = api.query_interface_settings(0)
 
 		if interface_descriptor != None:
-			print "bLength: " + str(interface_descriptor.b_length)
-			print "bDescriptorType: " + str(interface_descriptor.b_descriptor_type)
-			print "bInterfaceNumber: " + str(interface_descriptor.b_interface_number)
-			print "bAlternateSetting: " + str(interface_descriptor.b_alternate_setting)
-			print "bNumEndpoints " + str(interface_descriptor.b_num_endpoints)
-			print "bInterfaceClass " + str(interface_descriptor.b_interface_class)
-			print "bInterfaceSubClass: " + str(interface_descriptor.b_interface_sub_class)
-			print "bInterfaceProtocol: " + str(interface_descriptor.b_interface_protocol)
-			print "iInterface: " + str(interface_descriptor.i_interface)
+			print("bLength: " + str(interface_descriptor.b_length))
+			print("bDescriptorType: " + str(interface_descriptor.b_descriptor_type))
+			print("bInterfaceNumber: " + str(interface_descriptor.b_interface_number))
+			print("bAlternateSetting: " + str(interface_descriptor.b_alternate_setting))
+			print("bNumEndpoints " + str(interface_descriptor.b_num_endpoints))
+			print("bInterfaceClass " + str(interface_descriptor.b_interface_class))
+			print("bInterfaceSubClass: " + str(interface_descriptor.b_interface_sub_class))
+			print("bInterfaceProtocol: " + str(interface_descriptor.b_interface_protocol))
+			print("iInterface: " + str(interface_descriptor.i_interface))
 
-		pipe_info_list = map(api.query_pipe, range(interface_descriptor.b_num_endpoints))
-		for item in pipe_info_list:
-				print "PipeType: " + str(item.pipe_type)
-				print "PipeId: " + str(item.pipe_id)
-				print "MaximumPacketSize: " + str(item.maximum_packet_size)
-				print "Interval: " + str(item.interval)
+                pipe_info_list = list(map(api.query_pipe, list(range(interface_descriptor.b_num_endpoints))))
+                for item in pipe_info_list:
+				print("PipeType: " + str(item.pipe_type))
+				print("PipeId: " + str(item.pipe_id))
+				print("MaximumPacketSize: " + str(item.maximum_packet_size))
+				print("Interval: " + str(item.interval))
 
 		"""
-		buff = None -> Buffer length will be zero. No data expected
+                buff = None -> Buffer length will be zero. No data expected
 		"""
-		api.control_transfer(pkt1, buff=[0])
+                api.control_transfer(pkt1, buff=[0])
 		api.control_transfer(pkt2, buff=None)
 		api.control_transfer(pkt3, buff=[0])
 		api.control_transfer(pkt4, buff=[0])
@@ -98,10 +107,11 @@ if result > 0:
 		api.control_transfer(pkt18, buff=[0,0])
 		api.control_transfer(pkt19, buff=[0,0])
 		api.control_transfer(pkt20, buff=None)
-
+                
 		api.write(0x02, hello)
 		time.sleep(0.045)
-		api.write(0x02, header)
+                print(api.read(0x02, 5))
+                api.write(0x02, header)
 		time.sleep(0.380)
 		api.write(0x02, tx1)
 		time.sleep(0.380)
@@ -126,10 +136,10 @@ if result > 0:
 		api.write(0x02, tx11)
 		time.sleep(0.380)
 		api.write(0x02, tx12)
-
-
+                
+                
 	else:
-		print "PL2303 could not be init"
+                print("PL2303 could not be init")
 
 else:
-	print "No Usb devices connected"
+	print("No Usb devices connected")
